6f286f91810aaef60b3562a13e21c2170f42df9d
Sarah Peters <sarahpieters15@gmail.com>
2020-05-19 10:24:56 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-/^[AEIOUaeiou]/.match(word) ? true : false
+word.match?/^[AEIOUaeiou]/
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -15,5 +15,5 @@ text.match?(/^[A-Z].*\W$/)
 end
 
 def valid_phone_number?(phone)
-
+phone.scan?(/)
 end
